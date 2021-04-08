380d184b6c0dddeab0142ad2f0d88b860689892d
JamesM246 <james.maguire246@hotmail.co.uk>
2021-04-08 12:06:35 +0100

    Functional game with no dealer

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Card:
 
     # Returns integer value of card
     def get_value(self):
-        if self.value == 'A':
+        if self.value == 'A':   # Value should be interchangeable between 1 and 11
             return 1
         if self.value in ('J', 'Q', 'K'):
             return 10
@@ -44,26 +44,63 @@ class Hand:
     def clear_hand(self):
         self.hand = []
 
+    def draw(self):
+        self.hand.append(d.draw())
+
+    def hand_total(self):
+        return sum(map(lambda card: card.get_value(), self.hand))
+
 
 class Player(Hand):
-
     def __init__(self, name, balance=1000):
         Hand.__init__(self)
         self.name = name
         self.balance = balance
 
-    def place_bet(self):
-        pass
+
+def get_bet():
+    user_in = input('Enter bet amount: ')
+    return user_in
+
+
+def check_bust(total):
+    return total > 21
+
+
+def check_win(total):
+    if total <= 21:
+        return True
+
+
+def hit_stick():
+    print(f'Your cards: {player.print_hand()}')
+    print(f'Card total: {player.hand_total()}')
+    if check_bust(player.hand_total()):
+        print('Bust!')
+        return False
+    user_in = input('Hit or Stick? (h/s): ')
+    if user_in == 'h':
+        player.draw()
+        return hit_stick()
+    elif user_in == 's':
+        return check_win(player.hand_total())
+    else:
+        print('Invalid input.')
+        hit_stick()
 
 
 def play():
-    player.hand.append(d.draw())
-    player.hand.append(d.draw())
-    print(f'Your cards: {player.print_hand()}')
+    player.draw()
+    player.draw()
+    if hit_stick():
+        print('You win!')
+    else:
+        print('You lose.')
 
 
 if __name__ == '__main__':
     player = Player('player')
     d = Deck()
-    print(f"{player.name}'s balance: {player.balance}")
+    print(f"Your balance: {player.balance}")
+    bet = get_bet()
     play()
